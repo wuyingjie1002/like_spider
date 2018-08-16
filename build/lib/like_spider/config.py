da38b995ae6070e291ca7b97a623573f869d9d58
root <root@localhost.localdomain>
2018-08-16 21:00:52 +0800

    最新版本提交

--- build/lib/like_spider/config.py
+++ build/lib/like_spider/config.py
@@ -1,35 +1,25 @@
 """Project configuration file."""
-
-global DB_HOST
-global DB_USER
-global DB_PASSWORD
-global DB_NAME
-global DB_PORT
-global DB_CHARSET
-global PROXIES
-global HEADERS
-global LOG_DIR
-global EXCEL_DIR
+import sys
 
 """mysql database configuration """
-DB_HOST = 'localhost'
-DB_USER = 'root'
-DB_PASSWORD = 'root'
-DB_PORT = 3306
-DB_NAME = 'spider'
-DB_CHARSET = 'utf8'
+DB_HOST = sys.modules['DB_HOST']
+DB_USER = sys.modules['DB_USER']
+DB_PASSWORD = sys.modules['DB_PASSWORD']
+DB_PORT = sys.modules['DB_PORT']
+DB_NAME = sys.modules['DB_NAME']
+DB_CHARSET = sys.modules['DB_CHARSET']
 
 """roxy user-agent"""
-HEADERS = [{'User-Agent':'Mozilla/5.0 (Windows NT 6.1; W) Gecko/20100101 Firefox/59.0'}]
+HEADERS = sys.modules['HEADERS']
 
 """
 proxy ip:
 The proxy ip is randomly obtained through the PROXIES list. If it is not enabled, the real ip is used.
 """
-#PROXIES = [{'http':'61.136.163.245:8103'}]
+#PROXIES = sys.modules['PROXIES']
 
 """project execution log directory"""
-LOG_DIR = ''
+LOG_DIR = sys.modules['LOG_DIR']
 
 """excel file directory"""
-EXCEL_DIR = ''
+EXCEL_DIR = sys.modules['EXCEL_DIR']
